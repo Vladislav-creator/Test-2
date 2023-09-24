13670f0d0ed1f4e47358640911039f1b73541de6
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2023-09-24 20:20:02 +0300

    6

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 // Збереження ключа API в окремому файлі змінних
 
-axios.defaults.headers.common['Authorization'] = "RUutV9o6Wsaxt6MdwROcNBNPiyCS3A3gcUpA2RtcIDURKF9e1pdE1bDF";
+axios.defaults.headers.common['Authorization'] = "LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc";
 
 // Перевірка помилок під час виконання запиту до серверу
-axios.defaults.baseURL = "https://api.pexels.com/v1/search"
+axios.defaults.baseURL = "https://api.unsplash.com/search/photos"
 
 
 axios.interceptors.response.use(
@@ -19,7 +19,7 @@ axios.interceptors.response.use(
 
 async function fetchImages(query, page, perPage) {
   const response = await axios.get(
-    `?query=${query}&page=${page}&per_page=${perPage}`,
+    `?query=${query}&page=${page}&per_page=${perPage}& orientation='landscape',`,
   );
   return response.data; 
 }
